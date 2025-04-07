6483712eb34917c25d357c18e7bb951c22e0ffb8
Mariano Ocaranza <marianoocaranza@gmail.com>
2025-04-07 19:12:15 -0300

    landing page terminada

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,9 +3,9 @@ const navcentermobile = document.getElementById("nav-center-mobile")
 const burger = document.getElementById('burger');
 const mobileMenu = document.getElementById('mobilenav');
 const closeButton = document.getElementById('close-menu')
+const options = document.querySelectorAll('#menu-option-mobile')
 
-
-
+console.log(options)
 burger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
     closeButton.classList.toggle('active');
@@ -14,6 +14,13 @@ burger.addEventListener('click', () => {
 closeButton.addEventListener('click', ()=> {
     mobileMenu.classList.remove('active');
     closeButton.classList.remove('active')
+})
+
+options.forEach((option)=> {
+    option.addEventListener('click', ()=>{
+        mobileMenu.classList.remove('active');
+        closeButton.classList.remove('active')
+    })
 })
 
 window.onresize=()=> {
